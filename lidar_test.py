dd077e174cc86c8bc1d5597d97c6c469d70c945e
Jose <jefuentes@unal.edu.co>
2024-10-31 16:21:49 -0400

    Fixed Readme errors

--- lidar_test.py
+++ lidar_test.py
@@ -9,7 +9,7 @@ lidar_instance = rplidar.LidarWrapper("/dev/ttyUSB0", 1000000)
 print(pydoc.render_doc(rplidar))
 
 pygame.init()
-lcd = pygame.display.set_mode((800,800))
+lcd = pygame.display.set_mode((1000,1000))
 pygame.mouse.set_visible(False)
 lcd.fill((0,0,0))
 pygame.display.update()
@@ -21,10 +21,10 @@ def process_data(data):
         distance = data[angle]
         if distance > 0:                  # ignore initially ungathered data points
             max_distance = 5000
-            radians = angle * pi / 180.0
+            radians = (angle * pi / 180.0)  - pi/2  # -pi/2 to set the 0 degree up in the screen
             x = distance * cos(radians)
             y = distance * sin(radians)
-            point =(int(x*SCALE_FACTOR)+400,int(y*SCALE_FACTOR)+400)
+            point =(int(x*SCALE_FACTOR)+500,int(y*SCALE_FACTOR)+500)
             lcd.set_at(point, pygame.Color(255, 255, 255))
     pygame.display.update()
 
